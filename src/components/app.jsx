86baea588c8b95ac41c44ac1a1d3a3b3a852efc9
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-25 14:56:57 -0400

    'initialization of youtube_clone' react app. created 'index.js' and components folder in 'src' folder. created 'app.jsx' in components folder

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Testing</h1>
+            </div>
+        )
+    }
+}
+
+export default App;

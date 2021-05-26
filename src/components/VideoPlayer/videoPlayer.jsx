22604bb88cb6143ffee0a5f88a3072b8fdaa3d7e
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-26 15:35:08 -0400

    added 'videoId' and 'setVideoId' hooks to 'VideoPlayer' component.

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -1,13 +1,13 @@
-import React /*{useState}*/ from 'react';
+import React, {useState} /*{useState}*/ from 'react';
 import './videoPlayer.css';
 
 const VideoPlayer = () => {
 
-    //const [videoId, setVideoId] = useState('')
+    const [videoId, setVideoId] = useState('')
     
     return (
         <iframe title="view-player" id="ytplayer" type="text/html" 
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
         frameborder="0">
         </iframe>
     )

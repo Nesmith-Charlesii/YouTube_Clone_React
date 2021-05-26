30410519d146ba20c72613f4eeb7b0e0ba8e4548
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-25 20:30:04 -0400

    created 'CustomHooks' folder and 'useSearchForm' custom hook component. Imported 'useSearchForm' to searchBar.jsx

--- src/components/SearchBar/searchBar.jsx
+++ src/components/SearchBar/searchBar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const SearchBar = (props) => {
+    const [input, setInput] = useState('');
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+        setInput({
+            [event.targe.name]: event.target.value
+        })
+    }
+
     return (
         <form>
             <div className="form-group">
-                <input className="form-control" type="text" name="search" id="search" value={props.value}/>
+                <input className="form-control" type="text" name="input" id="input" onChange={(event) => handleChange(event)} value={input} placeholder="Search..." />
             </div>
         </form>
     )

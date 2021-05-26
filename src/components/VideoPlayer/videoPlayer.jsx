92d6554c055ee0f08a5bcf3600d56ad9cad9c7cd
Mike Peterson <mpeterson2004@gmail.com>
2021-05-26 15:10:51 -0600

    added default video to videoPlayer.jsx

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -3,10 +3,10 @@ import './videoPlayer.css';
 
 const VideoPlayer = () => {
 
-    const [videoId, setVideoId] = useState('')
+    const [videoId, setVideoId] = useState('44-Kx5ZZTsY')
     
     return (
-        <iframe title="view-player" id="ytplayer" type="text/html" 
+        <iframe title="view-player" id="ytplayer" type="text/html" width="640" height="360"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
         frameborder="0">
         </iframe>
